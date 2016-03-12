dddf2277a28a896c26be760e948857fe6cae5ab5
Tangerino <carlos.tangerino@gmail.com>
2016-03-12 15:39:30 +0100

    some updates and support for more features

--- wimd.py
+++ wimd.py
@@ -3,22 +3,19 @@ This is the WIMD.IO API wrapper
 '''
 import datetime
 from datetime import timedelta
-
+import sys
 import requests
 
 class wimd(object):
     BASE_URL = "http://wimd.io/v2/"
+    #BASE_URL = "http://192.168.0.27/v2/"
 
     def __init__(self):
         requests.packages.urllib3.disable_warnings()
         self.__socket = requests.session()
         self.__apikey = ''
         self.__apiheader = {'apikey': ''}
-        self.__isadmin = False
-        self.__cancreate = False
-        self.__canread = False
-        self.__canupdate = False
-        self.__candelete = False
+        self.__permissions = 0
 
     def url(self):
         return self.BASE_URL
@@ -27,19 +24,22 @@ class wimd(object):
         return self.__apikey
 
     def is_admin(self):
-        return self.__isadmin
+        if self.__permissions & 0x80:
+            return True
+        else:
+            return False
 
     def can_create(self):
-        return self.__cancreate
+        return True
 
     def can_read(self):
-        return self.__canread
+        return True
 
     def can_update(self):
-        return self.__canupdate
+        return True
 
     def can_delete(self):
-        return self.__candelete
+        return True
 
     def login(self, email, password):
         rc = False
@@ -50,11 +50,7 @@ class wimd(object):
                 resp = r.json()
                 self.__apikey = resp['apikey']
                 self.__apiheader = {'apikey': self.__apikey}
-                self.__cancreate = resp['permissions']['create']
-                self.__canread = resp['permissions']['read']
-                self.__canupdate = resp['permissions']['update']
-                self.__candelete = resp['permissions']['delete']
-                self.__isadmin = resp['permissions']['admin']
+                self.__permissions = resp['permissions']
                 rc = True
         except:
             print("Login error ")
@@ -85,13 +81,85 @@ class wimd(object):
             pass
         return False, {}
 
-    def things(self, placeid):
+    def place_new(self, name, description=''):
+        payload = {'name': name, 'description': description}
         try:
-            r = self.__socket.get(self.BASE_URL + 'place/' + placeid + '/things', headers=self.__apiheader,
-                                  verify=False)
+            r = self.__socket.post(self.BASE_URL + 'place', json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+
+    def place_delete(self, place_id):
+        try:
+            r = self.__socket.delete(self.BASE_URL + 'place/' + place_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+
+
+    def thing_new(self, place_id, name, description=''):
+        payload = {'name': name, 'description': description}
+        try:
+            r = self.__socket.post(self.BASE_URL + 'place/' + place_id + "/thing", json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            pass
+        return False, {}
+
+    def things_read(self, place_id):
+        try:
+            r = self.__socket.get(self.BASE_URL + 'place/' + place_id + '/things', headers=self.__apiheader, verify=False)
             return r.status_code == 200, r.json()
         except:
             pass
+        return False, {}
+
+    def thing_read(self, thing_id):
+        try:
+            r = self.__socket.get(self.BASE_URL + 'thing/' + thing_id, headers=self.__apiheader, verify=False)
+            return r.status_code == 200, r.json()
+        except:
+            pass
+        return False, {}
+
+    def thing_update(self, thing_id, place_id, name, description):
+        payload = {'placeid': place_id, 'name': name, 'description': description}
+        try:
+            r = self.__socket.put(self.BASE_URL + 'thing/' + thing_id, json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def thing_link_sensor(self, thing_id, sensor_id):
+        try:
+            r = self.__socket.post(self.BASE_URL + 'thing/' + thing_id + "/link/" + sensor_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def thing_unlink_sensor(self, thing_id, sensor_id):
+        try:
+            r = self.__socket.delete(self.BASE_URL + 'thing/' + thing_id + "/link/" + sensor_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def thing_delete(self, thing_id):
+        try:
+            r = self.__socket.delete(self.BASE_URL + 'thing/' + thing_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
         return False, {}
 
     def sensors(self, thing_id):
@@ -119,9 +187,9 @@ class wimd(object):
             r = self.__socket.post(self.BASE_URL + 'sensor', json=payload, headers=h, verify=False)
             if r.status_code == 201 or r.status_code == 200:
                 return True, r.json()
-        except Exception, e:
-            return false, str(e)
-        return false,{}
+        except:
+            return False, {sys.exc_info()[0]}
+        return False,{}
 
     def sensor_add_data(self, devkey, data):
         try:
@@ -148,6 +216,13 @@ class wimd(object):
         except:
             pass
         return False, {}
+
+    def sensor_rule_read(self, sensor_id):
+        try:
+            r = self.__socket.get(self.BASE_URL + 'sensor/' + sensor_id + "/rule", headers=self.__apiheader, verify=False)
+            return r.status_code == 200, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
 
     def device_new(self, mac, name, description=''):
         payload = {'mac': mac, 'name': name, 'description': description}
@@ -187,5 +262,71 @@ class wimd(object):
                 return True, r.json()
         except:
             pass
+        return False, {}
+
+    def device_update(self, deviceid, mac, name, description=''):
+        payload = {'mac': mac, 'name': name, 'description': description}
+        try:
+            r = self.__socket.put(self.BASE_URL + 'device/' + deviceid, json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def device_delete(self, deviceid):
+        try:
+            r = self.__socket.delete(self.BASE_URL + 'device/' + deviceid, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def etl_new(self, name, endpoint, username, password, type, placeid, database, table):
+        payload = {"name":name, "endpoint":endpoint, "username":username, "password":password, "type":type, "placeid":placeid, "database":database, "table":table}
+        try:
+            r = self.__socket.post(self.BASE_URL + 'etl', json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def etls_read(self):
+        try:
+            r = self.__socket.get(self.BASE_URL + 'etls', headers=self.__apiheader, verify=False)
+            if r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def etl_read(self, etl_id):
+        try:
+            r = self.__socket.get(self.BASE_URL + 'etl/' + etl_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def etl_update(self, etl_id, name, endpoint, username, password, type, placeid, database, table):
+        payload = {"name":name, "endpoint":endpoint, "username":username, "password":password, "type":type, "placeid":placeid, "database":database, "table":table}
+        try:
+            r = self.__socket.put(self.BASE_URL + 'etl/' + etl_id, json=payload, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
+        return False, {}
+
+    def etl_delete(self, etl_id):
+        try:
+            r = self.__socket.delete(self.BASE_URL + 'etl/' + etl_id, headers=self.__apiheader, verify=False)
+            if r.status_code == 201 or r.status_code == 200:
+                return True, r.json()
+        except:
+            return False, {sys.exc_info()[0]}
         return False, {}
 
